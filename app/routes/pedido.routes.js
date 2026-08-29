be14d26701fd4e858d26b708d6be67bf2a16270b
CARLOS DANIEL ROJAS ALVARADO <crojasa@miumg.edu.gt>
2026-08-29 10:59:39 -0600

    comentario

--- app/routes/pedido.routes.js
+++ app/routes/pedido.routes.js
@@ -2,6 +2,8 @@ module.exports = app => {
   const pedidos = require("../controllers/pedido.controller.js");
   var router = require("express").Router();
 
+  
+//Defininir, para diferenciar. 
   router.post("/", pedidos.create);
   router.get("/", pedidos.findAll);
 
