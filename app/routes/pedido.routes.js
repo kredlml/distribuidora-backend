403f5a9e1cd287ee96171b8df0d2ed575761fc3d
CARLOS DANIEL ROJAS ALVARADO <crojasa@miumg.edu.gt>
2026-09-10 23:06:13 -0600

    feat: protección completa de rutas de inventario y pedidos con roles RBAC

--- app/routes/pedido.routes.js
+++ app/routes/pedido.routes.js
@@ -1,13 +1,14 @@
 module.exports = app => {
   const pedidos = require("../controllers/pedido.controller.js");
+  const { verificarToken, isCajero } = require("../middlewares/authJwt.js"); 
   var router = require("express").Router();
 
+
+  router.post("/", [verificarToken, isCajero], pedidos.create);
   
-//Defininir, para diferenciar. 
-  router.post("/", pedidos.create);
-  router.get("/", pedidos.findAll);
-  router.get("/:id", pedidos.findOne);
+  
+  router.get("/", verificarToken, pedidos.findAll);
+  router.get("/:id", verificarToken, pedidos.findOne);
   
   app.use('/api/pedidos', router);
-  
 };
