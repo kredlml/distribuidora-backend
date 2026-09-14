58e1b6f76beea4903c4fb21e6894e2e34a141397
CARLOS DANIEL ROJAS ALVARADO <crojasa@miumg.edu.gt>
2026-09-13 21:31:47 -0600

    fix: corregir método a PUT en la ruta de actualizar estado

--- app/routes/pedido.routes.js
+++ app/routes/pedido.routes.js
@@ -3,12 +3,13 @@ module.exports = app => {
     const { verificarToken, isCajero } = require("../middlewares/authJwt.js");
     var router = require("express").Router();
 
-  router.get("/:id/estado", verificarToken, pedidos.actualizarEstado);
-  router.get("/:id/historial", verificarToken, pedidos.historial);
-  router.post("/", [verificarToken, isCajero], pedidos.create);
-  //router.put("/:id/estado", pedidos.updateEstado);  
-  router.get("/", verificarToken, pedidos.findAll);
-  router.get("/:id", verificarToken, pedidos.findOne);
+    
+    router.put("/:id/estado", verificarToken, pedidos.actualizarEstado);
   
-  app.use('/api/pedidos', router);
+    router.get("/:id/historial", verificarToken, pedidos.historial);
+    router.post("/", [verificarToken, isCajero], pedidos.create);
+    router.get("/", verificarToken, pedidos.findAll);
+    router.get("/:id", verificarToken, pedidos.findOne);
+  
+    app.use('/api/pedidos', router);
 };
